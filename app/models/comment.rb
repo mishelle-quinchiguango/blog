cb928698646fd2b35ead5a7e1dfdef8e896d9cda
mishelle-quinchiguango <mishelle.quinchiguango@epn.edu.ec>
2020-10-28 11:54:24 -0500

    validaciones

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validate_pesence_of :post_id
+    validate_pesence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     has_many:comments, dependent: :destroy
-    
+    validate_pesence_of :title
+    validate_pesence_of :body
 end
