cd8271f45514fa663d08761e4e32a2e4b5c24f89
mishelle-quinchiguango <mishelle.quinchiguango@epn.edu.ec>
2020-10-27 23:43:34 -0500

    migrateBdd

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_175642) do
+ActiveRecord::Schema.define(version: 2020_10_28_042619) do
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
+# Could not dump table "comments" because of following StandardError
+#   Unknown type 'reference' for column 'user'
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'reference' for column 'user'
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
